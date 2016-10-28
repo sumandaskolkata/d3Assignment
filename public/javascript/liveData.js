926e6e65df3ca099c6ae7451e8fc9d93a822e9d4
sumandaskolkata <sumandas.kolkata@yahoo.com>
2016-10-28 09:50:29 +0530

    adding transition to the line graph

--- public/javascript/liveData.js
+++ public/javascript/liveData.js
@@ -1,26 +1,20 @@
-var data = [20,4,10,6,23,7,10,5,55,9];
+var _data = [20,4,10,6,23,7,10,5,55,9];
 
 const NUMBER_OF_DATA = 10
 
-const WIDTH = 800;
+const WIDTH = 900;
 const HEIGHT = 600;
 const MARGIN = 30;
 
 const INNER_WIDTH = WIDTH - (2 * MARGIN);
 const INNER_HEIGHT = HEIGHT - (2 * MARGIN);
 
-var generateNewData = function(){
-  var newData = _.random(1,100);
-  data.push(newData);
-  data.shift();
-}
-
 var translate = function(x, y){
 	return "translate("+x+","+y+")";
 };
 
 var _xScale = d3.scaleLinear()
-  .domain([0,NUMBER_OF_DATA-1])
+  .domain([0,_data.length-1])
   .range([0,INNER_WIDTH]);
 
 var _yScale = d3.scaleLinear()
@@ -38,7 +32,7 @@ var generateChart = function(){
   //adding svg
   var svg = d3.select('.container').append('svg')
   .attr("height",HEIGHT)
-  .attr("width",WIDTH);
+  .attr("width",WIDTH-MARGIN)
 
   var xAxis = d3.axisBottom(_xScale).ticks(10);
   var yAxis = d3.axisLeft(_yScale).ticks(10);
@@ -46,63 +40,62 @@ var generateChart = function(){
   svg.append('g')
     .attr('transform', translate(MARGIN,MARGIN))
     .call(yAxis)
-    .attr("class","yAxis");
   //adding xAxis
   svg.append('g')
     .attr('transform', translate(MARGIN,HEIGHT-MARGIN))
     .call(xAxis)
-    .attr("class","xAxis");
     
   var g = svg.append('g')
     .attr('transform', translate(MARGIN,MARGIN))
-    .classed("path",true);
+    .classed("line_chart",true);
   
   //creating bars
-  g.selectAll('rect').data(data)
-    .enter().append('rect')
-    .attr('fill','black')
-    .attr('width',2)    
-    .attr('x',function(entry,index){return _xScale(index)})
-    .attr('y',function(entry){return _yScale(entry)})    
-    .attr('height',function(entry){return INNER_HEIGHT - _yScale(entry)} );
+  // g.selectAll('rect').data(data)
+  //   .enter().append('rect')
+  //   .attr('fill','skyblue')
+  //   .attr('width',7)    
+  //   .attr('x',function(entry,index){return _xScale(index)})
+  //   .attr('y',function(entry){return _yScale(entry)})    
+  //   .attr('height',function(entry){return INNER_HEIGHT - _yScale(entry)} );
 
   //creating path  
   g.append('path')
     .classed("line",true)
-    .attr('d',_line(data));
+    .datum(_data)
+    .attr('d',_line);
 
-    setInterval(callback,250);
+    setInterval(update,850);
 }
 
 var updateChart = function(){
-  var g = d3.selectAll('svg .path');
-  //data binding
-  var path = g.selectAll('path').data(data);
-  
-  //update
-  path.classed("line",true)
-    .attr('d',_line(data));
+  _data.push(_.random(1,100));
 
-  //remove  
-  path.exit().remove();
+  d3.select('svg .line_chart')
+  .select('path').datum(_data)
+    .attr('d',_line)
+    .attr('transform',null)
+    .transition()
+    .duration(800)
+    .ease(d3.easeLinear)
+    .attr('transform',translate(_xScale(-1),0));
 
+  _data.shift();
 
-  //data binding
-  var rects = g.selectAll('rect').data(data);
-  //enter
-  rects.enter().append('rect')
-    .attr('width',4);
-  //update  
-  rects.attr('fill','black')
-    .attr('x',function(entry,index){return _xScale(index)})
-    .attr('y',function(entry){return _yScale(entry)})    
-    .attr('height',function(entry){return INNER_HEIGHT - _yScale(entry)} )
-  //remove
-  rects.exit().remove();
+  // //data binding
+  // var rects = g.selectAll('rect').data(data);
+  // //enter
+  // rects.enter().append('rect')
+  //   .attr('width',7);
+  // //update  
+  // rects.attr('fill','skyblue')
+  //   .attr('x',function(entry,index){return _xScale(index)})
+  //   .attr('y',function(entry){return _yScale(entry)})    
+  //   .attr('height',function(entry){return INNER_HEIGHT - _yScale(entry)} )
+  // //remove
+  // rects.exit().remove();
 }
 
-var callback = function(){
-  generateNewData();
+var update = function(){
   updateChart();
 }
 
