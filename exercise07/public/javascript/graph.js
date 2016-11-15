e7dcee595693f2cbfc827d10ff5d5a1f1c82c3aa
sumandaskolkata <sumandas.kolkata@yahoo.com>
2016-11-16 00:25:52 +0530

    line chart is created with the points

---
+++ exercise07/public/javascript/graph.js
@@ -0,0 +1,38 @@
+var points = [{x:0,y:5},{x:1,y:9},{x:2,y:7},{x:3,y:5},{x:4,y:3},{x:6,y:4},{x:7,y:2},{x:8,y:3},{x:9,y:2}];
+
+var xScale = d3.scaleLinear()
+            .domain([0,10])
+            .range([20,420]);
+
+var yScale = d3.scaleLinear()
+            .domain([0,10])
+            .range([420,20]);
+var line = d3.line()
+    .x(function(d){return xScale(d.x); })
+    .y(function(d){return yScale(d.y); });
+
+var loadAxis = function(svg){
+    var xAxis = d3.axisBottom(xScale).ticks(12);
+	var yAxis = d3.axisLeft(yScale).ticks(10);
+
+	svg.append('g')
+		.attr('transform', 'translate(0,420)')
+		.call(xAxis);
+
+	svg.append('g')
+		.attr('transform', 'translate(20,0)')
+		.call(yAxis);
+}
+var loadGraph = function(){
+ var svg = d3.select("body").append("svg")
+        .attr("height","500")
+        .attr("width","500");
+    loadAxis(svg);
+   
+    svg.append("path").datum(points)
+        .classed("line",true)
+        .attr("d",line);
+}
+
+
+window.onload = loadGraph;
