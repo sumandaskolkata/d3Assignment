105799e4549a6a0c82a15c6b230e5a79a3725d02
sumandaskolkata <sumandas.kolkata@yahoo.com>
2016-11-18 10:36:13 +0530

    exercies 08 new method created for drawing circle on data point

--- exercies08/public/javascript/lineGraph.js
+++ exercies08/public/javascript/lineGraph.js
@@ -1,42 +1,38 @@
 var points = [{x:0,y:5},{x:1,y:9},{x:2,y:7},{x:3,y:5},{x:4,y:3},{x:6,y:4},{x:7,y:2},{x:8,y:3},{x:9,y:2}];
 
-var xScale = d3.scaleLinear().domain([0,1]).range([30,430]);
-
-var yScale = d3.scaleLinear().domain([0,1]).range([430,30]);
-
-var line = d3.line()
-    .x(function(d){return xScale(d.x/10); })
-    .y(function(d){return yScale(d.y/10); });
-
-var sinValue  = new Array(10);
-var sinLine = d3.line()
-    .x(function(d,i){return xScale(i/10); })
-    .y(function(d,i){return yScale((Math.sin(i)/10)+0.5) });
-
-var loadAxis = function(svg){
+var loadAxis = function(svg,xScale,yScale){
     var xAxis = d3.axisBottom(xScale).ticks(10);
 	var yAxis = d3.axisLeft(yScale).ticks(10);
-
 	svg.append('g')
 		.attr('transform', 'translate(0,430)')
 		.call(xAxis);
-
 	svg.append('g')
 		.attr('transform', 'translate(30,0)')
 		.call(yAxis);
 }
 
-var loadSineGraph = function(svg){
+var loadSineGraph = function(svg,xScale,yScale){
+    var sinValue  = new Array(10);
+    var sinLine = d3.line()
+        .x(function(d,i){return xScale(i/10); })
+        .y(function(d,i){return yScale((Math.sin(i)/10)+0.5) });
+
     svg.append("path").datum(sinValue)
         .classed("sinGraph",true)
         .attr("d",sinLine);
 }
-var loadLineGraph = function(svg){
+
+var loadLineGraph = function(svg,xScale,yScale){
+    var line = d3.line()
+    .x(function(d){return xScale(d.x/10); })
+    .y(function(d){return yScale(d.y/10); });
+
      svg.append("path").datum(points)
         .classed("line",true)
         .attr("d",line);
 }
-var createLineDataPoints = function(svg){
+
+var createLineDataPoints = function(svg,xScale,yScale){
     var paths = svg.append("g").selectAll("path").data(points).enter().append("path");
     paths
         .classed("data_points",true)
@@ -45,7 +41,8 @@ var createLineDataPoints = function(svg){
             return "translate("+xScale(d.x/10)+","+yScale(d.y/10)+")"
         })
 }
-var createSinDataPoints = function(svg){
+
+var createSinDataPoints = function(svg,xScale,yScale){
     var paths = svg.append("g").selectAll("path").data(sinValue).enter().append("path");
     paths
         .classed("data_points",true)
@@ -55,15 +52,16 @@ var createSinDataPoints = function(svg){
         })
 }
 var loadGraph = function(){
+    var xScale = d3.scaleLinear().domain([0,1]).range([30,430]);
+    var yScale = d3.scaleLinear().domain([0,1]).range([430,30]);
     var svg = d3.select("body").append("svg")
         .attr("height","500")
         .attr("width","500");
-    loadAxis(svg);
-    loadSineGraph(svg);
-    loadLineGraph(svg);
-    createLineDataPoints(svg);
-    createSinDataPoints(svg);
+    loadAxis(svg,xScale,yScale);
+    loadSineGraph(svg,xScale,yScale);
+    loadLineGraph(svg,xScale,yScale);
+    createLineDataPoints(svg,xScale,yScale);
+    createSinDataPoints(svg,xScale,yScale);
 }
-
 
 window.onload = loadGraph;
